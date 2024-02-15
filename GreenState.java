9cf5a5d2a8cf02dfa54e55282afb10005de52288
Jeremias Esperanza <142370600+JerryEsperanza@users.noreply.github.com>
2024-02-15 01:23:31 +0000

    Updated the files!

---
+++ GreenState.java
@@ -0,0 +1,10 @@
+public class GreenState implements TrafficLightState{
+    public void switchState(TrafficLightController controller){
+        controller.setState(new YellowState());
+
+    }
+    
+    public String getColor(){
+        return "Green";
+    }
+}
---
+++ RedState.java
@@ -0,0 +1,10 @@
+public class RedState implements TrafficLightState{
+    public void switchState(TrafficLightController controller){
+        controller.setState(new GreenState());
+
+    }
+    
+    public String getColor(){
+        return "Red";
+    }
+}
